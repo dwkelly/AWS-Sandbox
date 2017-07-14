34e614e61dcbf5314587b8d490d888c1796db3f7
DWK <d.w.kelly88@gmail.com>
2017-07-14 13:55:34 -0400

    Updated the to consume message and print all fields

--- aws-squarespace-prototype/ConsumeSqsMessage.js
+++ aws-squarespace-prototype/ConsumeSqsMessage.js
@@ -1,3 +1,8 @@
+/**
+The receives messags off an SQS queue and prints them to the localhost:8080. The fields that will be included are orderId, orderSubtotal,orderSubtotalCents, orderGrandTotal, orderGrandTotalCents, and orderGrandTotalCentsorderGrandTotalCentsorderGrandTotalCents
+
+**/
+
 var AWS = require('aws-sdk');
 var http = require('http');
 
@@ -9,30 +14,57 @@ var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
 const queueUrl = "https://sqs.us-east-1.amazonaws.com/399091925902/test_queue";
 
 var resultString ="";
-var result;
+var result= {};
 
 var params = {
 	MaxNumberOfMessages: 10,
-	MessageAttributeNames: ["orderId","orderSubtotal","orderSubtotalCents","orderGrandTotal","orderGrandTotalCents","customerEmailAddress"], 
+	MessageAttributeNames: ["All"], 
+//	MessageAttributeNames: ["orderId","orderSubtotal","orderSubtotalCents","orderGrandTotal","orderGrandTotalCents","customerEmailAddress"], 
 	QueueUrl: queueUrl,
-	VisibilityTimeout: 10,
-	WaitTimeSeconds: 0
+	VisibilityTimeout: 0,	
+	WaitTimeSeconds: 10
 };
 
-sqs.receiveMessage(params, function(err, data){
-	if (err){
-		console.log("Receive Error", err);
-	} else {
-		console.log("Data Received");
-		resultString= data.toString();
-		result= data;
-	}
-});
+function receiveMessage (){
+		sqs.receiveMessage(params, function(err, data){
+		if (err){
+			console.log("Receive Error", err);
+		} else {
+			console.log("Data Received");
+			resultString= data.toString();
+			result = data;
+		}
+	});
+}
+receiveMessage();
+
 
 const server = http.createServer(function (req, res){
+		receiveMessage();
 		res.writeHead(200, {'Content-type': 'text/html'});
-//		res.end(result.Messages);
-		res.end(new Buffer(JSON.stringify(result)));
+
+		JSON.stringify(result);
+		var printString ="";
+		var i;
+		for (i=0 ; i< result.Messages.length; i++){
+//			printString+= JSON.stringify(result.Messages[i].MessageAttributes);
+			//console.log(JSON.stringify(result.Messages[i].MessageAttributes));
+			console.log(result.Messages[i].MessageAttributes);
+			if(result.Messages[i].MessageAttributes.customerEmailAddress!= null){
+				printString+= "result "+ i;
+				printString+= "<br/>orderId: "+result.Messages[i].MessageAttributes.orderId.StringValue;
+				printString+= "<br/>customerEmailAddress: "+result.Messages[i].MessageAttributes.customerEmailAddress.StringValue;
+				printString+= "<br/>orderSubtotal: "+result.Messages[i].MessageAttributes.orderSubtotal.StringValue;
+				printString+= "<br/>orderSubtotalCents: "+result.Messages[i].MessageAttributes.orderSubtotalCents.StringValue;
+				printString+= "<br/>orderGrandTotal: "+result.Messages[i].MessageAttributes.orderGrandTotalCents.StringValue;
+				printString+= "<br/>orderGrandTotal: "+result.Messages[i].MessageAttributes.orderGrandTotal.StringValue+"<br/><br/><br/>";
+			}
+		}
+		
+//		res.end(new Buffer(JSON.stringify(result.Messages)+printString));
+		res.end(new Buffer(printString));
+
+
 }).listen(8080);
 
 console.log("Running over http://locahost:8080");
